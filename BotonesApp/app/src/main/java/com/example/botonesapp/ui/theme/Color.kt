8f7f69186eb81f77265bc483b61e911108def647
Jose Garcia <josegarcia19809@gmail.com>
2026-02-28 20:45:19 -0600

    Android: Botones, temas y colores - Temas y Colores

--- BotonesApp/app/src/main/java/com/example/botonesapp/ui/theme/Color.kt
+++ BotonesApp/app/src/main/java/com/example/botonesapp/ui/theme/Color.kt
@@ -9,3 +9,27 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Primary = Color(0xFF7C3AED)        // Violeta moderno
+val PrimaryContainer = Color(0xFFEDE9FE)
+
+val Secondary = Color(0xFF06B6D4)      // Cyan moderno
+val SecondaryContainer = Color(0xFFCFFAFE)
+
+val Tertiary = Color(0xFFF59E0B)       // Amarillo acento moderno
+
+val Success = Color(0xFF22C55E)
+val ErrorCustom = Color(0xFFEF4444)
+val Warning = Color(0xFFF97316)
+val Info = Color(0xFF3B82F6)
+
+val BackgroundLight = Color(0xFFF8FAFC)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceVariantLight = Color(0xFFE2E8F0)
+
+val BackgroundDark = Color(0xFF0F172A)
+val SurfaceDark = Color(0xFF1E293B)
+val SurfaceVariantDark = Color(0xFF334155)
+
+val PrimaryDark = Color(0xFFA78BFA)
+val SecondaryDark = Color(0xFF22D3EE)
