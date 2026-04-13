793a138ee4d4aea728fe1d71dea0db45b5495c68
Jose Garcia <josegarcia19809@gmail.com>
2026-04-13 16:28:56 -0600

    Componente Image y Icon: Imágenes avanzadas

--- BotonesApp/app/src/main/java/com/example/botonesapp/components/Imagenes.kt
+++ BotonesApp/app/src/main/java/com/example/botonesapp/components/Imagenes.kt
@@ -1,19 +1,28 @@
 package com.example.botonesapp.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.botonesapp.R
 
 @Composable
-fun Imagenes(paddingValues: PaddingValues){
+fun Imagenes(paddingValues: PaddingValues) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -24,6 +33,19 @@ fun Imagenes(paddingValues: PaddingValues){
         Image(
             painter = painterResource(R.drawable.venado2),
             contentDescription = "image profile"
+        )
+
+        Image(
+            painter = painterResource(R.drawable.venado2),
+            contentDescription = "image profile",
+            modifier = Modifier
+                .size(300.dp)
+                .clip(RoundedCornerShape(50))
+                .border(
+                    width = 4.dp, shape = CircleShape,
+                    brush = Brush.linearGradient(colors = listOf(Color.Red, Color.White))
+                ),
+            contentScale = ContentScale.FillBounds
         )
     }
 }
