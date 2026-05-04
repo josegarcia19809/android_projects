282aa5276e5ca6112c4e2124ad6d4158efb52bf9
Jose Garcia <josegarcia19809@gmail.com>
2026-05-04 07:18:08 -0600

    Componente ProgressBar: LinearProgressIndicator y ProgressIndicator avanzado

--- BotonesApp/app/src/main/java/com/example/botonesapp/components/Progress.kt
+++ BotonesApp/app/src/main/java/com/example/botonesapp/components/Progress.kt
@@ -1,8 +1,24 @@
 package com.example.botonesapp.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,9 +29,11 @@ fun Progress(
     modifier: Modifier = Modifier,
     color: Color = Color.Blue
 ) {
-    Box(
+    var progress by remember { mutableFloatStateOf(0.5f) }
+    Column(
         modifier = modifier.fillMaxSize(), // ocupa todo el espacio disponible
-        contentAlignment = Alignment.Center // centra el contenido
+        verticalArrangement = Arrangement.Center, // centra el contenido
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         CircularProgressIndicator(
             Modifier.size(140.dp),
@@ -23,5 +41,27 @@ fun Progress(
             strokeWidth = 15.dp,
             trackColor = Color.LightGray
         )
+        Spacer(Modifier.height(24.dp))
+        LinearProgressIndicator(
+            color = Color.Red,
+            strokeCap = ProgressIndicatorDefaults.LinearStrokeCap,
+            trackColor = Color.LightGray
+        )
+
+        Spacer(Modifier.height(24.dp))
+        CircularProgressIndicator(
+            progress = { progress },
+            Modifier.size(140.dp),
+            color = Color.Red,
+            strokeWidth = 15.dp,
+            trackColor = Color.LightGray
+        )
+        Spacer(Modifier.height(24.dp))
+        Row(Modifier.padding(24.dp)) {
+            Button(onClick = { progress += 0.1f }) { Text("<-") }
+            Spacer(Modifier.width(24.dp))
+            Button(onClick = { progress -= 0.1f }) { Text("->") }
+        }
+
     }
 }
